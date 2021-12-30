f8ac0fc30100cee56935641f9d41a5c2b7e309b4
Gregory Bers <gregorybers@epiecs.be>
2021-12-30 13:43:18 +0100

    default image settings can now be specified in the meta.yml file

--- resources/scripts/defaultimagesettings.py
+++ resources/scripts/defaultimagesettings.py
@@ -1,10 +1,14 @@
 #! /usr/bin/python3
 import re
 import sys
+import yaml
 from pprint import pprint
 """
     Set default image settings if not present
 """
+
+with open('meta.yml', 'r') as yaml_file:
+    meta_settings = yaml.load(yaml_file, Loader=yaml.SafeLoader)
 
 with open(sys.argv[1], "r+") as file:
     data = file.read()
@@ -14,7 +18,7 @@ with open(sys.argv[1], "r+") as file:
     # matches = set(re.findall(regex_images, data, flags=re.MULTILINE))   
     # pprint(matches)
 
-    data = re.sub(regex_images, r"\g<1>{width=60%}\n", data, flags=re.MULTILINE)
+    data = re.sub(regex_images, fr"\g<1>{{width={meta_settings['default-image-width']}%}}\n", data, flags=re.MULTILINE)
     
     file.seek(0)
     file.write(data)
